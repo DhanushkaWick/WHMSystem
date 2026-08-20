7494057d47e15109d6ac8f0a4369776b430b0b1f
DhanushkaWick <dmdhanushkamadhuranga@gmail.com>
2026-08-20 12:32:14 +0530

    ll

--- src/main/java/AbstractComponent/AbstractComponent.java
+++ src/main/java/AbstractComponent/AbstractComponent.java
@@ -2,25 +2,21 @@ package AbstractComponent;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class AbstractComponent {
-    WebDriver driver;
+
+    protected WebDriver driver;
 
     public AbstractComponent(WebDriver driver) {
-
         this.driver = driver;
-       // PageFactory.initElements(driver, this);
     }
 
     public void waitForWEbElementToAppear(WebElement findBy) {
-
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
         wait.until(ExpectedConditions.visibilityOf(findBy));
     }
-
 }
