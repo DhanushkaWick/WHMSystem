8f73e2810244c90f64f2d3b9e07933f5add43980
DhanushkaWick <dmdhanushkamadhuranga@gmail.com>
2026-08-20 12:32:14 +0530

    ll

--- src/main/java/PageObject/LoginPage.java
+++ src/main/java/PageObject/LoginPage.java
@@ -8,16 +8,13 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.xml.xpath.XPath;
 import java.time.Duration;
 
 public class LoginPage extends AbstractComponent {
-    WebDriver driver;
 
     public LoginPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
-
     }
 
     @FindBy(xpath = "//input[@id='email']")
